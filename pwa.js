6e42d337c2ce2c55f36e3a97c6efcc130e841c60
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-24 18:34:35 -0300

    Organiza pwa.js em js/

--- pwa.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator){
-  window.addEventListener('load',async()=>{
-    try{
-      const reg=await navigator.serviceWorker.register('./sw.js');
-      reg.update();
-      navigator.serviceWorker.addEventListener('controllerchange',()=>console.log('Mecânica do Zé: nova versão ativa'));
-    }catch(e){console.warn('SW error',e);}
-  });
-}
