732b5a2b9f0a5db1b6a765959cab6480bd4b9b68
jogoiarexx-blip <jogoiarexx@gmail.com>
2026-09-25 15:00:39 -0300

    v3.1: atualiza config e Save v6

--- js/game-config.js
+++ js/game-config.js
@@ -1,6 +1,6 @@
 window.MZ_CONFIG = Object.freeze({
-  VERSION: '3.0.0',
-  SAVE_VERSION: 5,
+  VERSION: '3.1.0',
+  SAVE_VERSION: 6,
   BASE_HUNGER_DRAIN: 0.008,
   WORKDAY_HUNGRY_TICKS: 12 * 60 * 4,
   MAX_DPR: 2
